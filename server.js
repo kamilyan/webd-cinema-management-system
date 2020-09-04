d99c394129d985d952661640cb68abc6c1ba2856
Maor Kamilyan <MaorKamilyan@gmail.com>
2020-09-04 09:20:36 +0300

    separation of concerns

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./server/config/app');
 var debug = require('debug')('cinema-management-system:server');
 var http = require('http');
 
--- server/config/app.js
+++ server/config/app.js
@@ -5,29 +5,29 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var menuRouter = require('./routes/menu');
-var createMovieRouter = require('./routes/movies/createMovie');
-var searchMovieRouter = require('./routes/movies/searchMovie');
-var searchResultsRouter = require('./routes/movies/searchResults');
-var movieDataRouter = require('./routes/movies/movieData');
-var usersManagementRouter = require('./routes/users/usersManagement');
-var userDataRouter = require('./routes/users/userData');
-const isAuthenticated = require('./middlewares/authentication.js')
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+var loginRouter = require('../routes/login');
+var menuRouter = require('../routes/menu');
+var createMovieRouter = require('../routes/movies/createMovie');
+var searchMovieRouter = require('../routes/movies/searchMovie');
+var searchResultsRouter = require('../routes/movies/searchResults');
+var movieDataRouter = require('../routes/movies/movieData');
+var usersManagementRouter = require('../routes/users/usersManagement');
+var userDataRouter = require('../routes/users/userData');
+const isAuthenticated = require('../middlewares/authentication.js')
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(session({
   secret: 'some secret',
